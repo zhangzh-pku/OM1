7b8205f4a29b6ca27fdf22bb7dcea44e8f8f695c
Kyle <kyle@openmind.org>
2025-01-25 19:26:04 -0800

    fix prompt to only react to new ETH transaction

--- src/inputs/plugins/wallet_coinbase.py
+++ src/inputs/plugins/wallet_coinbase.py
@@ -55,10 +55,10 @@ class WalletCoinbase(LoopInput[float]):
         await asyncio.sleep(self.POLL_INTERVAL)
 
         # randomly simulate ETH inbound transfers for debugging purposes
-        if random.randint(0, 10) > 7:
-            faucet_transaction = self.wallet.faucet(asset_id='eth')
-            faucet_transaction.wait()
-            logging.info(f"WalletCoinbase: Faucet transaction: {faucet_transaction}")
+        # if random.randint(0, 10) > 7:
+        #     faucet_transaction = self.wallet.faucet(asset_id='eth')
+        #     faucet_transaction.wait()
+        #     logging.info(f"WalletCoinbase: Faucet transaction: {faucet_transaction}")
 
         self.wallet = Wallet.fetch(self.COINBASE_WALLET_ID)
         logging.info(f"WalletCoinbase: Wallet refreshed: {self.wallet.balance('eth')}, the current balance is {self.ETH_balance}")
@@ -76,7 +76,7 @@ class WalletCoinbase(LoopInput[float]):
         if balance_change > 0:
             message = f"{time.time():.3f}::You just received {balance_change:.5f} ETH."
         else:
-            message = f"{time.time():.3f}::There is no new ETH transaction. balance: {balance:.5f}"
+            message = f"{time.time():.3f}::There is no new ETH transaction."
 
         logging.info(f"WalletCoinbase: {message}")
         return Message(timestamp=time.time(), message=message)
