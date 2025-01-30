2126ad61d50399c27020ad4e8f60be78d6ad594c
Boya Ouyang <42460189+sdoouyangboya@users.noreply.github.com>
2025-01-30 12:54:36 -0800

    Formatted code with black

--- src/actions/tweet/interface.py
+++ src/actions/tweet/interface.py
@@ -6,10 +6,12 @@ from actions.base import Interface
 @dataclass
 class TweetInput:
     """Input interface for tweet action."""
+
     tweet: str = ""  # Make tweet optional with default empty string
 
 
 class Tweet(Interface):
     """Tweet action interface."""
+
     input: TweetInput
     output: TweetInput
