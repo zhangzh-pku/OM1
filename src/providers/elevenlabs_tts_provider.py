90e09c574709ec8ab7e5f9c2a5c71e27cfcc9739
bchen-dev <boyuan.eth@gmail.com>
2025-02-26 15:04:04 -0800

    Custom code is not needed

--- src/providers/elevenlabs_tts_provider.py
+++ src/providers/elevenlabs_tts_provider.py
@@ -1,84 +1,11 @@
-import base64
-import io
 import logging
 import threading
 import time
 from typing import Callable, Optional
 
 from om1_speech import AudioOutputStream
-from pydub import AudioSegment
 
 from .singleton import singleton
-
-
-class ElevenLabsAudioOutputStream(AudioOutputStream):
-    """
-    Audio output stream for Eleven Labs TTS service.
-
-    This class extends the base AudioOutputStream class to provide
-    additional functionality for the Eleven Labs TTS service.
-    """
-
-# 44100 allows the G81 and the Bose to work
-#  8000 allows the internal audio card to work 
-
-
-    def __init__(
-        self,
-        url,
-        rate=44100, # used to be 8000 for basic iris
-        device=None,
-        device_name=None,
-        tts_state_callback=None,
-        headers=None,
-    ):
-        super().__init__(url, rate, device, device_name, tts_state_callback, headers)
-
-    def _write_audio(self, audio_data: bytes):
-        """
-        Override the base class method to write audio data from the Eleven Labs TTS service.
-        """
-
-        self._tts_callback(True)
-
-        audio_bytes = base64.b64decode(audio_data)
-
-        audio_segment = AudioSegment.from_mp3(io.BytesIO(audio_bytes))
-        raw_data = audio_segment.raw_data
-
-        current_format = self.stream._format
-        needed_format = self._audio_interface.get_format_from_width(
-            audio_segment.sample_width
-        )
-        audio_chunk = int(audio_segment.frame_rate * 0.1) # 100ms of audio
-
-        if (
-            current_format != needed_format
-            or self.stream._channels != audio_segment.channels
-            or self.stream._rate != audio_segment.frame_rate
-        ):
-            self.stream.stop_stream()
-            self.stream.close()
-
-            # Reopen the stream with the new format
-            self.stream = self._audio_interface.open(
-                output_device_index=self._device,
-                format=needed_format,
-                channels=audio_segment.channels,
-                rate=audio_segment.frame_rate,
-                output=True,
-                frames_per_buffer=audio_chunk,
-            )
-
-        for i in range(0, len(raw_data), audio_chunk):
-            self.stream.write(
-                raw_data[i : i + audio_chunk], exception_on_underflow=False
-            )
-
-        self.stream.stop_stream()
-        self.stream.start_stream()
-        self._tts_callback(False)
-
 
 @singleton
 class ElevenLabsTTSProvider:
@@ -126,7 +53,7 @@ class ElevenLabsTTSProvider:
         # Initialize TTS provider
         self.running: bool = False
         self._thread: Optional[threading.Thread] = None
-        self._audio_stream: ElevenLabsAudioOutputStream = ElevenLabsAudioOutputStream(
+        self._audio_stream: AudioOutputStream = AudioOutputStream(
             url=url,
             device=device_id,
             device_name=speaker_name,
