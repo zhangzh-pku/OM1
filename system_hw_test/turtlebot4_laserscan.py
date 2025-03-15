44685eb0c76e902ba2b6edfd28706dfcf247f105
TMC <TangmereCottage@protonmail.com>
2025-03-15 14:35:32 -0700

    correct configuration of turtleBot4 and the Zenoh bridge

---
+++ system_hw_test/turtlebot4_laserscan.py
@@ -0,0 +1,28 @@
+import time
+
+import zenoh
+
+"""
+ros2 topic echo /pi/scan
+"""
+
+
+def listener(sample):
+    print(
+        f"Received {sample.kind} ('{sample.key_expr}': '{sample.payload.to_string()}')"
+    )
+
+
+if __name__ == "__main__":
+
+    # initiate logging
+    # zenoh.init_logger()
+    # configure zenoh in peer mode
+    # conf = zenoh.Config.from_json5('{"mode": "peer"}')
+
+    with zenoh.open(zenoh.Config()) as session:
+        print("Zenoh is open")
+        sub = session.declare_subscriber("pi/scan", listener)
+        while True:
+            print("Waiting for pi/scan messages")
+            time.sleep(1)
