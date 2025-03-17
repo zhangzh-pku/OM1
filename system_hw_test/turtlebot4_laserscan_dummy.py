677fb706b501c98e51a6b3d1b2ac61e30cdb2c6d
TMC <TangmereCottage@protonmail.com>
2025-03-16 21:33:15 -0700

    update docs and run lint/ruff/black

--- system_hw_test/turtlebot4_laserscan_dummy.py
+++ system_hw_test/turtlebot4_laserscan_dummy.py
@@ -1,13 +1,18 @@
-import zenoh, random, time
+import random
+import time
+
+import zenoh
 
 random.seed()
+
 
 def read_temp():
     return random.randint(15, 30)
 
+
 if __name__ == "__main__":
     with zenoh.open(zenoh.Config()) as session:
-        key = 'pi/scan2'
+        key = "pi/scan"
         pub = session.declare_publisher(key)
         while True:
             t = read_temp()
