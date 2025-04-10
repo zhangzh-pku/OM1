be015cfcd5946f0f6ecafe8a4d2457533febafec
TMC <TangmereCottage@protonmail.com>
2025-04-10 12:32:09 -0700

    Update gps_mag.ino

--- system_hw_test/gps_mag/gps_mag.ino
+++ system_hw_test/gps_mag/gps_mag.ino
@@ -17,9 +17,18 @@ uint32_t timerMAG = millis();
 
 int IMU_LAST_READ = 0;
 
-bool CALIBRATION = true;
-// the serial data need to be injected into 
-// https://github.com/PaulStoffregen/MotionCal
+/* For calibration, set this to true, run the script, close
+the Arduino IDE, close the serial monitor, and open MotionCal
+
+https://github.com/PaulStoffregen/MotionCal
+
+If you do not see your Arduino in the device Dropdown menu,
+then follow these instructions to compile MotionCal for your 
+OS and platform
+
+https://github.com/PaulStoffregen/MotionCal/issues/11#issuecomment-2412937251
+*/
+bool CALIBRATION = false;
 
 float ToDecimalDegrees(float formattedLatLon)
 {
@@ -30,14 +39,28 @@ float ToDecimalDegrees(float formattedLatLon)
   return decDegrees + fractDegrees;
 }
 
+void PrintFloatArray(float ar[], int n)
+{
+  for(int i = 0; i < n; i++) {
+    Serial.print(ar[i]);
+    if(i == n - 1) {
+      Serial.print("\n");
+    } else {
+      Serial.print(":");
+    }
+  }
+}
+
 // These are specific to each IMU and IMU environment - 
 // Poor calibration values will prevent AHRS function
-float mag_hardiron[]  = { 24.42, -16.87, -3.88 }; // in uTesla
-float mag_softiron[]  = { \
-0.959, 0.050, -0.038, \
-0.050, 1.078, 0.002,  \
--0.038, 0.002, 0.970 }; 
-float gyro_zerorate[] = { 0.00, 0.02, 0.01 }; // in Radians/s
+// Add your device specific values below, in the init loop
+float mag_hardiron[]  = { 0.0, 0.0, 0.0 }; // in uTesla
+float mag_softiron[]  = { 1.0, 0.0, 0.0, \
+                          0.0, 1.0, 0.0, \
+                          0.0, 0.0, 1.0 }; 
+float gyro_zerorate[] = { 0.0, 0.0, 0.0 }; // in Radians/s
+
+//declination = +12° 50'
 
 #include "quaternionFilters.h"
 #define MPS2_TO_G 9.80665
@@ -47,8 +70,33 @@ void setup()
   // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
   // also spit it out
   Serial.begin(115200);
-  while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+  while (!Serial) delay(10); // pause until serial console opens
   
+  char DeviceID[9];
+  itoa(NRF_FICR->DEVICEID[0], DeviceID, 16);
+  Serial.print("Device ID 0: ");
+  Serial.println(DeviceID);
+
+  if (strcmp(DeviceID, "38e4bfd6") == 0) {
+    float mh[] = {24.420, -16.87, -3.880}; // in uTesla
+    float ms[] = { 0.959,  0.050, -0.038,  \
+                   0.050,  1.078,  0.002,  \
+                  -0.038,  0.002,  0.970}; 
+    float gc[] = { 0.000,  0.020,  0.010}; // in Radians/s
+    memcpy(mag_hardiron,  mh, sizeof(mag_hardiron));
+    memcpy(mag_softiron,  ms, sizeof(mag_softiron));
+    memcpy(gyro_zerorate, gc, sizeof(gyro_zerorate));
+  } else {
+    Serial.println("CAUTION: Magnetometer not calibrated - code will yield garbage - please calibrate your Magnetometer and IMU");
+  }
+
+  Serial.println("Using these calibration values:");
+  PrintFloatArray(mag_hardiron,  3);
+  PrintFloatArray(mag_softiron,  9);
+  PrintFloatArray(gyro_zerorate, 3);
+
+  delay(5000);
+
   Serial.println("GPS booting");
 
   GPS.begin(9600);
@@ -63,61 +111,19 @@ void setup()
 
   Serial.println("Booting LIS3MDL");
   
-  // Try to initialize!
-  if (! lis3mdl.begin_I2C()) {          // hardware I2C mode, can pass in address & alt Wire
-    Serial.println("Failed to find LIS3MDL chip");
+  if (! lis3mdl.begin_I2C()) {
+    Serial.println("Failed to find LIS3MDL");
     while (1) { delay(10); }
   }
-  Serial.println("LIS3MDL Found!");
+  Serial.println("LIS3MDL Found");
 
   lis3mdl.setPerformanceMode(LIS3MDL_MEDIUMMODE);
-  Serial.print("Performance mode set to: ");
-  switch (lis3mdl.getPerformanceMode()) {
-    case LIS3MDL_LOWPOWERMODE: Serial.println("Low"); break;
-    case LIS3MDL_MEDIUMMODE: Serial.println("Medium"); break;
-    case LIS3MDL_HIGHMODE: Serial.println("High"); break;
-    case LIS3MDL_ULTRAHIGHMODE: Serial.println("Ultra-High"); break;
-  }
-
   lis3mdl.setOperationMode(LIS3MDL_CONTINUOUSMODE);
-  Serial.print("Operation mode set to: ");
-  // Single shot mode will complete conversion and go into power down
-  switch (lis3mdl.getOperationMode()) {
-    case LIS3MDL_CONTINUOUSMODE: Serial.println("Continuous"); break;
-    case LIS3MDL_SINGLEMODE: Serial.println("Single mode"); break;
-    case LIS3MDL_POWERDOWNMODE: Serial.println("Power-down"); break;
-  }
-
   lis3mdl.setDataRate(LIS3MDL_DATARATE_155_HZ);
-  // You can check the datarate by looking at the frequency of the DRDY pin
-  Serial.print("Data rate set to: ");
-  switch (lis3mdl.getDataRate()) {
-    case LIS3MDL_DATARATE_0_625_HZ: Serial.println("0.625 Hz"); break;
-    case LIS3MDL_DATARATE_1_25_HZ: Serial.println("1.25 Hz"); break;
-    case LIS3MDL_DATARATE_2_5_HZ: Serial.println("2.5 Hz"); break;
-    case LIS3MDL_DATARATE_5_HZ: Serial.println("5 Hz"); break;
-    case LIS3MDL_DATARATE_10_HZ: Serial.println("10 Hz"); break;
-    case LIS3MDL_DATARATE_20_HZ: Serial.println("20 Hz"); break;
-    case LIS3MDL_DATARATE_40_HZ: Serial.println("40 Hz"); break;
-    case LIS3MDL_DATARATE_80_HZ: Serial.println("80 Hz"); break;
-    case LIS3MDL_DATARATE_155_HZ: Serial.println("155 Hz"); break;
-    case LIS3MDL_DATARATE_300_HZ: Serial.println("300 Hz"); break;
-    case LIS3MDL_DATARATE_560_HZ: Serial.println("560 Hz"); break;
-    case LIS3MDL_DATARATE_1000_HZ: Serial.println("1000 Hz"); break;
-  }
-  
   lis3mdl.setRange(LIS3MDL_RANGE_4_GAUSS);
-  Serial.print("Range set to: ");
-  switch (lis3mdl.getRange()) {
-    case LIS3MDL_RANGE_4_GAUSS: Serial.println("+-4 gauss"); break;
-    case LIS3MDL_RANGE_8_GAUSS: Serial.println("+-8 gauss"); break;
-    case LIS3MDL_RANGE_12_GAUSS: Serial.println("+-12 gauss"); break;
-    case LIS3MDL_RANGE_16_GAUSS: Serial.println("+-16 gauss"); break;
-  }
-
   lis3mdl.setIntThreshold(500);
   lis3mdl.configInterrupt(false, false, true, // enable z axis
-                          true, // polarity
+                          true,  // polarity
                           false, // don't latch
                           true); // enabled!
 
@@ -125,146 +131,25 @@ void setup()
 
   if (!lsm6ds3trc.begin_I2C()) {
     Serial.println("Failed to find LSM6DS3TR-C chip");
-    while (1) {
-      delay(10);
-    }
+    while (1) { delay(10); }
   }
 
-  Serial.println("LSM6DS3TR-C Found!");
+  Serial.println("LSM6DS3TR-C Found");
 
-  // lsm6ds3trc.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (lsm6ds3trc.getAccelRange()) {
-  case LSM6DS_ACCEL_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case LSM6DS_ACCEL_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case LSM6DS_ACCEL_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case LSM6DS_ACCEL_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
-
-  // lsm6ds3trc.setGyroRange(LSM6DS_GYRO_RANGE_250_DPS);
-  Serial.print("Gyro range set to: ");
-  switch (lsm6ds3trc.getGyroRange()) {
-  case LSM6DS_GYRO_RANGE_125_DPS:
-    Serial.println("125 degrees/s");
-    break;
-  case LSM6DS_GYRO_RANGE_250_DPS:
-    Serial.println("250 degrees/s");
-    break;
-  case LSM6DS_GYRO_RANGE_500_DPS:
-    Serial.println("500 degrees/s");
-    break;
-  case LSM6DS_GYRO_RANGE_1000_DPS:
-    Serial.println("1000 degrees/s");
-    break;
-  case LSM6DS_GYRO_RANGE_2000_DPS:
-    Serial.println("2000 degrees/s");
-    break;
-  case ISM330DHCX_GYRO_RANGE_4000_DPS:
-    break; // unsupported range for the DS33
-  }
-
-  // lsm6ds3trc.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
-  Serial.print("Accelerometer data rate set to: ");
-  switch (lsm6ds3trc.getAccelDataRate()) {
-  case LSM6DS_RATE_SHUTDOWN:
-    Serial.println("0 Hz");
-    break;
-  case LSM6DS_RATE_12_5_HZ:
-    Serial.println("12.5 Hz");
-    break;
-  case LSM6DS_RATE_26_HZ:
-    Serial.println("26 Hz");
-    break;
-  case LSM6DS_RATE_52_HZ:
-    Serial.println("52 Hz");
-    break;
-  case LSM6DS_RATE_104_HZ:
-    Serial.println("104 Hz");
-    break;
-  case LSM6DS_RATE_208_HZ:
-    Serial.println("208 Hz");
-    break;
-  case LSM6DS_RATE_416_HZ:
-    Serial.println("416 Hz");
-    break;
-  case LSM6DS_RATE_833_HZ:
-    Serial.println("833 Hz");
-    break;
-  case LSM6DS_RATE_1_66K_HZ:
-    Serial.println("1.66 KHz");
-    break;
-  case LSM6DS_RATE_3_33K_HZ:
-    Serial.println("3.33 KHz");
-    break;
-  case LSM6DS_RATE_6_66K_HZ:
-    Serial.println("6.66 KHz");
-    break;
-  }
-
-  // lsm6ds3trc.setGyroDataRate(LSM6DS_RATE_12_5_HZ);
-  Serial.print("Gyro data rate set to: ");
-  switch (lsm6ds3trc.getGyroDataRate()) {
-  case LSM6DS_RATE_SHUTDOWN:
-    Serial.println("0 Hz");
-    break;
-  case LSM6DS_RATE_12_5_HZ:
-    Serial.println("12.5 Hz");
-    break;
-  case LSM6DS_RATE_26_HZ:
-    Serial.println("26 Hz");
-    break;
-  case LSM6DS_RATE_52_HZ:
-    Serial.println("52 Hz");
-    break;
-  case LSM6DS_RATE_104_HZ:
-    Serial.println("104 Hz");
-    break;
-  case LSM6DS_RATE_208_HZ:
-    Serial.println("208 Hz");
-    break;
-  case LSM6DS_RATE_416_HZ:
-    Serial.println("416 Hz");
-    break;
-  case LSM6DS_RATE_833_HZ:
-    Serial.println("833 Hz");
-    break;
-  case LSM6DS_RATE_1_66K_HZ:
-    Serial.println("1.66 KHz");
-    break;
-  case LSM6DS_RATE_3_33K_HZ:
-    Serial.println("3.33 KHz");
-    break;
-  case LSM6DS_RATE_6_66K_HZ:
-    Serial.println("6.66 KHz");
-    break;
-  }
-
+  lsm6ds3trc.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
+  lsm6ds3trc.setGyroRange(LSM6DS_GYRO_RANGE_250_DPS);
+  lsm6ds3trc.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
+  lsm6ds3trc.setGyroDataRate(LSM6DS_RATE_12_5_HZ);
   lsm6ds3trc.configInt1(false, false, true); // accelerometer DRDY on INT1
   lsm6ds3trc.configInt2(false, true, false); // gyro DRDY on INT2
 }
 
-void loop() // run over and over again
+void loop()
 {
-  // read data from the GPS in the 'main loop'
+
   char c = GPS.read();
-  // // if you want to debug, this is a good time to do it!
-  // if (GPSECHO)
-  //   if (c) Serial.print(c);
-  
   // if a sentence is received, we can check the checksum, parse it...
   if (GPS.newNMEAreceived()) {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences!
-    // so be very wary if using OUTPUT_ALLDATA and trying to print out data
-    //Serial.print(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
     if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
       return; // we can fail to parse a sentence in which case we should just wait for another
   }
@@ -272,29 +157,7 @@ void loop() // run over and over again
   // approximately every 1 seconds or so, print out the current stats
   if (millis() - timerGPS > 1000) {
     timerGPS = millis(); // reset the timer
-    
-    // Serial.print("\nGPS Time: ");
-    
-    // if (GPS.hour < 10) { Serial.print('0'); }
-    // Serial.print(GPS.hour, DEC); Serial.print(':');
-    // if (GPS.minute < 10) { Serial.print('0'); }
-    // Serial.print(GPS.minute, DEC); Serial.print(':');
-    // if (GPS.seconds < 10) { Serial.print('0'); }
-    // Serial.print(GPS.seconds, DEC); Serial.print('.');
-    // if (GPS.milliseconds < 10) {
-    //   Serial.print("00");
-    // } else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) {
-    //   Serial.print("0");
-    // }
-
-    // Serial.println(GPS.milliseconds);
-    // Serial.print("Date: ");
-    // Serial.print(GPS.day, DEC); Serial.print('/');
-    // Serial.print(GPS.month, DEC); Serial.print("/20");
-    // Serial.println(GPS.year, DEC);
-    // Serial.print("Fix: "); Serial.print((int)GPS.fix);
-    // Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
-
+  
     if (GPS.fix && CALIBRATION == false) {
 
       float latDecimal = ToDecimalDegrees(GPS.latitude);
@@ -309,7 +172,7 @@ void loop() // run over and over again
       // Serial.print(GPS.longitude, 4); 
       // Serial.print(GPS.lon);
       
-      // use this is you want decimal degrees
+      // use this if you want decimal degrees
       Serial.print("GPS:");
       Serial.print(latDecimal,6);
       Serial.print(GPS.lat);
@@ -383,36 +246,36 @@ void loop() // run over and over again
       return;
     }
 
-    Serial.print("T:");
-    Serial.print(temp.temperature);
-    Serial.println("C");
+    // Serial.print("T:");
+    // Serial.print(temp.temperature);
+    // Serial.println("C");
 
-    /* Display the results (acceleration is measured in m/s^2) */
-    Serial.print("AX:");
-    Serial.print(accel.acceleration.x);
-    Serial.print("Y:");
-    Serial.print(accel.acceleration.y);
-    Serial.print("Z:");
-    Serial.print(accel.acceleration.z);
-    Serial.println(" m/s^2");
+    // /* Display the results (acceleration is measured in m/s^2) */
+    // Serial.print("AX:");
+    // Serial.print(accel.acceleration.x);
+    // Serial.print("Y:");
+    // Serial.print(accel.acceleration.y);
+    // Serial.print("Z:");
+    // Serial.print(accel.acceleration.z);
+    // Serial.println(" m/s^2");
 
-    /* Display the results (rotation is measured in rad/s) */
-    Serial.print("GX:");
-    Serial.print(gyro.gyro.x);
-    Serial.print("Y:");
-    Serial.print(gyro.gyro.y);
-    Serial.print("Z:");
-    Serial.print(gyro.gyro.z);
-    Serial.println(" rad/s");
+    // /* Display the results (rotation is measured in rad/s) */
+    // Serial.print("GX:");
+    // Serial.print(gyro.gyro.x);
+    // Serial.print("Y:");
+    // Serial.print(gyro.gyro.y);
+    // Serial.print("Z:");
+    // Serial.print(gyro.gyro.z);
+    // Serial.println(" rad/s");
 
-    /* Display the results (magnetic field is measured in uTesla) */
-    Serial.print("MAG:X:"); Serial.print(mag.magnetic.x);
-    Serial.print("Y:"); Serial.print(mag.magnetic.y); 
-    Serial.print("Z:"); Serial.print(mag.magnetic.z); 
-    Serial.println("uT");
+    // /* Display the results (magnetic field is measured in uTesla) */
+    // Serial.print("MAG:X:"); Serial.print(mag.magnetic.x);
+    // Serial.print("Y:"); Serial.print(mag.magnetic.y); 
+    // Serial.print("Z:"); Serial.print(mag.magnetic.z); 
+    // Serial.println("uT");
 
-    Serial.print("DT(s):");
-    Serial.println(imu_dt_s);
+    // Serial.print("DT(s):");
+    // Serial.println(imu_dt_s);
 
     float ax_G = accel.acceleration.x / MPS2_TO_G;
     float ay_G = accel.acceleration.y / MPS2_TO_G;
@@ -434,12 +297,12 @@ void loop() // run over and over again
 
     MahonyQuaternionUpdate(ax_G, ay_G, az_G, gx, gy, gz, mxCal, myCal, mzCal, imu_dt_s);
 
-    String dataLog = String("DATA:");
-    dataLog += String(" AX:") + String(ax_G) + String(",AY:") + String(ay_G) + String(",AZ:") + String(az_G);
-    dataLog += String(",GX:") + String(gx) + String(",GY:") + String(gy) + String(",GZ:") + String(gz);
-    dataLog += String(",MX:") + String(mxCal) + String(",MY:") + String(myCal) + String(",MZ:") + String(mzCal);
+    // String dataLog = String("DATA:");
+    // dataLog += String(" AX:") + String(ax_G) + String(",AY:") + String(ay_G) + String(",AZ:") + String(az_G);
+    // dataLog += String(",GX:") + String(gx) + String(",GY:") + String(gy) + String(",GZ:") + String(gz);
+    // dataLog += String(",MX:") + String(mxCal) + String(",MY:") + String(myCal) + String(",MZ:") + String(mzCal);
 
-    Serial.println(dataLog);
+    // Serial.println(dataLog);
 
     // NOTE: Written by Kris Winer
     // Define output variables from updated quaternion---these are Tait-Bryan
@@ -469,79 +332,80 @@ void loop() // run over and over again
                   * *(getQ()+1) - *(getQ()+2) * *(getQ()+2) + *(getQ()+3)
                   * *(getQ()+3));
   
-    Serial.print("Yaw, Pitch, Roll: ");
+    Serial.print("YPR: ");
     Serial.print(myIMUyaw *= RAD_TO_DEG, 2);
     Serial.print(", ");
     Serial.print(myIMUpitch *= RAD_TO_DEG, 2);
     Serial.print(", ");
     Serial.println(myIMUroll *= RAD_TO_DEG, 2);
 
-    String cardinal;
+  int headingDegrees = 0.0;
+  if (myIMUyaw < 0) {
+    headingDegrees = -1.0 * myIMUyaw;
+  } else {
+    headingDegrees = -1.0 * myIMUyaw + 360.0;
+  }
 
-    float headingRadians = atan2(myCal, mxCal);
-    // flipped is correct?
+  String cardinal;
+  if (headingDegrees > 348.75 || headingDegrees < 11.25) {
+    cardinal = " N";
+  }
+  else if (headingDegrees > 11.25 && headingDegrees < 33.75) {
+    cardinal = " NNE";
+  }
+  else if (headingDegrees > 33.75 && headingDegrees < 56.25) {
+    cardinal = " NE";
+  }
+  else if (headingDegrees > 56.25 && headingDegrees < 78.75) {
+    cardinal = " ENE";
+  }
+  else if (headingDegrees > 78.75 && headingDegrees < 101.25) {
+    cardinal = " E";
+  }
+  else if (headingDegrees > 101.25 && headingDegrees < 123.75) {
+    cardinal = " ESE";
+  }
+  else if (headingDegrees > 123.75 && headingDegrees < 146.25) {
+    cardinal = " SE";
+  }
+  else if (headingDegrees > 146.25 && headingDegrees < 168.75) {
+    cardinal = " SSE";
+  }
+  else if (headingDegrees > 168.75 && headingDegrees < 191.25) {
+    cardinal = " S";
+  }
+  else if (headingDegrees > 191.25 && headingDegrees < 213.75) {
+    cardinal = " SSW";
+  }
+  else if (headingDegrees > 213.75 && headingDegrees < 236.25) {
+    cardinal = " SW";
+  }
+  else if (headingDegrees > 236.25 && headingDegrees < 258.75) {
+    cardinal = " WSW";
+  }
+  else if (headingDegrees > 258.75 && headingDegrees < 281.25) {
+    cardinal = " W";
+  }
+  else if (headingDegrees > 281.25 && headingDegrees < 303.75) {
+    cardinal = " WNW";
+  }
+  else if (headingDegrees > 303.75 && headingDegrees < 326.25) {
+    cardinal = " NW";
+  }
+  else if (headingDegrees > 326.25 && headingDegrees < 348.75) {
+    cardinal = " NNW";
+  }
 
-    float headingDegrees = headingRadians * 180 / PI;
-    float declinationAngle = 0.0;
+  float headingRadiansNTC = atan2(myCal, mxCal);
+  float headingDegreesNTC = headingRadiansNTC * 180 / PI;
+  if (headingDegreesNTC < 0) {
+    headingDegreesNTC += 360;
+  }
 
-    headingDegrees += declinationAngle;
-
-    if (headingDegrees < 0) {
-      headingDegrees += 360;
-    }
-
-    if (headingDegrees > 348.75 || headingDegrees < 11.25) {
-      cardinal = " N";
-    }
-    else if (headingDegrees > 11.25 && headingDegrees < 33.75) {
-      cardinal = " NNE";
-    }
-    else if (headingDegrees > 33.75 && headingDegrees < 56.25) {
-      cardinal = " NE";
-    }
-    else if (headingDegrees > 56.25 && headingDegrees < 78.75) {
-      cardinal = " ENE";
-    }
-    else if (headingDegrees > 78.75 && headingDegrees < 101.25) {
-      cardinal = " E";
-    }
-    else if (headingDegrees > 101.25 && headingDegrees < 123.75) {
-      cardinal = " ESE";
-    }
-    else if (headingDegrees > 123.75 && headingDegrees < 146.25) {
-      cardinal = " SE";
-    }
-    else if (headingDegrees > 146.25 && headingDegrees < 168.75) {
-      cardinal = " SSE";
-    }
-    else if (headingDegrees > 168.75 && headingDegrees < 191.25) {
-      cardinal = " S";
-    }
-    else if (headingDegrees > 191.25 && headingDegrees < 213.75) {
-      cardinal = " SSW";
-    }
-    else if (headingDegrees > 213.75 && headingDegrees < 236.25) {
-      cardinal = " SW";
-    }
-    else if (headingDegrees > 236.25 && headingDegrees < 258.75) {
-      cardinal = " WSW";
-    }
-    else if (headingDegrees > 258.75 && headingDegrees < 281.25) {
-      cardinal = " W";
-    }
-    else if (headingDegrees > 281.25 && headingDegrees < 303.75) {
-      cardinal = " WNW";
-    }
-    else if (headingDegrees > 303.75 && headingDegrees < 326.25) {
-      cardinal = " NW";
-    }
-    else if (headingDegrees > 326.25 && headingDegrees < 348.75) {
-      cardinal = " NNW";
-    }
-
-    Serial.print("NON_COMP Heading: ");
-    Serial.print(headingDegrees);
-    Serial.println(cardinal);
-
+  Serial.print("HDG (DEG): ");
+  Serial.print(headingDegrees);
+  Serial.print(cardinal);
+  Serial.print(" NTC_HDG: ");
+  Serial.println(headingDegreesNTC);
   }
 }
