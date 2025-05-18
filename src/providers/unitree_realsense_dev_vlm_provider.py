81fc5561828320a43489227cee2c45948daa8825
Kyle <kyle@openmind.org>
2025-05-17 20:08:36 -0700

    Fix VLM provider VideoStream register_frame_callback

--- src/providers/unitree_realsense_dev_vlm_provider.py
+++ src/providers/unitree_realsense_dev_vlm_provider.py
@@ -296,7 +296,7 @@ class UnitreeRealSenseDevVLMProvider:
 
         if self.stream_ws_client:
             self.stream_ws_client.start()
-            self.video_stream.register_stream_callback(
+            self.video_stream.register_frame_callback(
                 self.stream_ws_client.send_message
             )
 
