08fb4b99c8fca6129386e6e9b0e5b7531761f1aa
TMC <TangmereCottage@protonmail.com>
2025-06-03 17:53:50 -0700

    lint

--- system_hw_test/test_audio_mac.py
+++ system_hw_test/test_audio_mac.py
@@ -1,10 +1,10 @@
+import osascript
 import sounddevice as sd
 import soundfile as sf
-import osascript
 
-filename = 'woof.wav'
+filename = "woof.wav"
 # Extract data and sampling rate from file
-data, fs = sf.read(filename, dtype='float32')  
+data, fs = sf.read(filename, dtype="float32")
 
 # Get all available devices
 devices = sd.query_devices()
@@ -14,17 +14,21 @@ print(devices)
 # You might need to change the index based on your system
 # You can also set it using a string, e.g., sd.default.device = 'Internal Speakers'
 try:
-    output_device_index = next(i for i, device in enumerate(devices) if device['name'] == "External Headphones")
+    output_device_index = next(
+        i for i, device in enumerate(devices) if device["name"] == "External Headphones"
+    )
     sd.default.device = output_device_index
     print(f"Default output device set to: {devices[output_device_index]['name']}")
 except StopIteration:
     print("No output device found.")
 
 # Verify the change
-print(f"Current default output device is: {sd.query_devices(sd.default.device)['name']}")
+print(
+    f"Current default output device is: {sd.query_devices(sd.default.device)['name']}"
+)
 
-print(f"Current volume settings")
-result = osascript.osascript('get volume settings')
+print("Current volume settings")
+result = osascript.osascript("get volume settings")
 print(result)
 
 target_volume = 100
@@ -32,14 +36,14 @@ print(f"Setting volume to: {target_volume}")
 vol = "set volume output volume " + str(target_volume)
 osascript.osascript(vol)
 
-result = osascript.osascript('get volume settings')
+result = osascript.osascript("get volume settings")
 print(result)
 
 # force unmute
-osascript.osascript('set volume without output muted')
+osascript.osascript("set volume without output muted")
 
-print(f"Confirm loud and unmuted:")
-result = osascript.osascript('get volume settings')
+print("Confirm loud and unmuted:")
+result = osascript.osascript("get volume settings")
 print(result)
 
 sd.play(data, fs)
