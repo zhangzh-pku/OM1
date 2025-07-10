796b93788e602387bbd7152ecb370e4b32c88136
openminddev <147775420+openminddev@users.noreply.github.com>
2025-07-10 13:44:46 -0700

    Fix Linux mp for cyclonedds

--- src/run.py
+++ src/run.py
@@ -1,4 +1,5 @@
 import asyncio
+import multiprocessing as mp
 
 import dotenv
 import typer
@@ -23,5 +24,8 @@ def start(config_name: str, log_level: str = "INFO", log_to_file: bool = False) 
 
 
 if __name__ == "__main__":
+    # Fix for multiprocessing on Linux
+    mp.set_start_method("spawn")
+
     dotenv.load_dotenv()
     app()
